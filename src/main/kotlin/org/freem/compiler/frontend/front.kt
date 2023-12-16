bac60e01bfa34a393d5c561a8ba1ad1917c2a953
PhillY <philly706869@gmail.com>
2023-12-16 17:35:57 +0900

    commit

--- src/main/kotlin/org/freem/compiler/frontend/front.kt
+++ src/main/kotlin/org/freem/compiler/frontend/front.kt
@@ -1,1 +1,74 @@
 package org.freem.compiler.frontend
+
+sealed interface P<Target>: Iterator<Target> {
+    fun scope(vararg blocks: P<Target>.() -> Unit)
+    fun scope(block: P<Target>.() -> Unit)
+
+    fun capture(block: P<Target>.() -> Unit): Array<Target>
+
+    fun need(target: Target, interruptMessage: String? = null)
+    fun need(interruptMessage: String? = null, condition: (target: Target) -> Boolean)
+    fun interrupt(message: String? = null): Nothing
+}
+
+fun P<Char>.need(target: String, interruptMessage: String? = null) {
+    scope {
+        for (char in target) need(char)
+    }
+}
+
+private class PPP<Target>(private val array: Array<Target>, var index: Int): P<Target> {
+
+    override fun hasNext(): Boolean = index < array.size
+    override fun next(): Target = array[index++]
+
+    override fun scope(block: P<Target>.() -> Unit) {
+        val indexBackup = index
+        try {
+            block()
+        } catch (throwable: Throwable) {
+            index = indexBackup
+            throw throwable
+        }
+    }
+
+    override fun scope(vararg blocks: P<Target>.() -> Unit) {
+        if (blocks.isEmpty()) return
+
+        for (block in blocks) {
+            try {
+                scope(block)
+                return
+            } catch (interruption: PInterruption) {
+                continue
+            }
+        }
+
+        interrupt()
+    }
+
+    override fun capture(block: P<Target>.() -> Unit): Array<Target> {
+        val startIndex = index
+        block()
+        return array.sliceArray(startIndex..<index)
+    }
+
+    override fun need(target: Target, interruptMessage: String?) = need(interruptMessage) { it == target }
+    override fun need(interruptMessage: String?, condition: (target: Target) -> Boolean) {
+        if (!hasNext() || !condition(next())) interrupt(interruptMessage)
+    }
+
+    override fun interrupt(message: String?): Nothing = throw PInterruption(message)
+}
+
+class PP<Target>(val block: P<Target>.() -> Unit) {
+
+    fun run(array: Array<Target>) {
+        val ppp = PPP(array, 0)
+        ppp.block()
+        if (ppp.index != array.size) throw IllegalStateException("array item remained")
+    }
+
+}
+
+class PInterruption(override val message: String?): Throwable()
--- src/test/kotlin/commonTest.kt
+++ src/test/kotlin/commonTest.kt
@@ -1,4 +1,15 @@
+import org.freem.compiler.frontend.PP
 
 fun main() {
+    var capturedStr = ""
 
+    PP<Char> {
+        capturedStr = capture {
+            scope {
+                while (hasNext()) if (!next().isDigit()) break
+            }
+        }.joinToString("")
+    }.run("43213".toList().toTypedArray())
+
+    println(capturedStr)
 }
