5b5e28d5fe9e5e43d7a715be7314eab2c78b9bdb
PhillY <philly706869@gmail.com>
2023-12-20 21:35:53 +0900

    commit

--- src/test/kotlin/commonTest.kt
+++ src/test/kotlin/commonTest.kt
@@ -1,15 +1,80 @@
-import org.freem.compiler.frontend.PP
+import org.freem.compiler.frontend.Partition
+import org.freem.compiler.frontend.PartitionField
 
 fun main() {
-    var capturedStr = ""
 
-    PP<Char> {
-        capturedStr = capture {
-            scope {
-                while (hasNext()) if (!next().isDigit()) break
+}
+
+object TestNameSpace {
+    class File private constructor(
+        val name: String,
+        val package_: Package,
+        val imports: List<Package>,
+    ) {
+        companion object: Partition<File> {
+            override fun PartitionField.execute(): File {
+                TODO("Not yet implemented")
             }
-        }.joinToString("")
-    }.run("43213".toList().toTypedArray())
+        }
+    }
 
-    println(capturedStr)
+    class Package private constructor(
+
+    ) {
+        companion object: Partition<Package> {
+            override fun PartitionField.execute(): Package {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    class Identifier private constructor(val value: String) {
+
+
+        companion object: Partition<Identifier> {
+            override fun PartitionField.execute(): Identifier {
+                return Identifier("")
+            }
+        }
+    }
+
+    class Class private constructor(val name: String) {
+
+
+        companion object: Partition<Class> {
+            override fun PartitionField.execute(): Class {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    class Function private constructor(val name: String, val returnType: Type) {
+
+
+        companion object: Partition<Function> {
+            override fun PartitionField.execute(): Function {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    class Type private constructor() {
+
+
+        companion object: Partition<Type> {
+            override fun PartitionField.execute(): Type {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    class Expression private constructor() {
+
+
+        companion object: Partition<Expression> {
+            override fun PartitionField.execute(): Expression {
+                TODO("Not yet implemented")
+            }
+        }
+    }
 }
