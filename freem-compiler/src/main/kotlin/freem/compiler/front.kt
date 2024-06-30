ad8d0fb52dd44b67cc67b9c78d51e65a3b0b5162
PhillY <philly706869@gmail.com>
2024-06-30 23:20:06 +0900

    commit

---
+++ freem-compiler/src/main/kotlin/freem/compiler/front.kt
@@ -0,0 +1,1 @@
+package freem.compiler
--- libfsp/src/main/kotlin/libfsp/node.kt
+++ libfsp/src/main/kotlin/libfsp/node.kt
@@ -1,136 +1,319 @@
 package libfsp
 
-import libfsp.Node.Parser
-import java.util.*
+sealed class FSPNode<Type, Return> {
 
-class Node<Input, Return, Error> {
+    fun parse(input: Array<out Type>): Return {
+        return parse()
+    }
 
-}
-sealed class Element
+    fun parse(input: Iterable<Type>): Return {
+        return parse()
+    }
 
-sealed class Node<Input, Return, Error> {
-    fun parse(input: Array<Input>) = parse_(input.asList())
-    fun parse(input: ArrayList<Input>) = parse_(input)
-    private fun parse_(input: List<Input>) {
-        val parser = createParser(input, 0)
-        val result = parser.parse()
-        when (result) {
-            is ParseSucceed -> return TODO("get return value from return node value")
-            is ParseFailure ->
+    fun parse(input: Iterator<Type>): Return {
+        return parse()
+    }
+
+    private fun parse(): Return {
+        TODO()
+    }
+
+    protected abstract fun createParser(parseNode: ParseNode): Parser
+
+    protected class ParseNode {
+        val parent: ParseNode
+
+        constructor() {
+            this.parent = this
         }
+
+        private constructor(parent: ParseNode) {
+            this.parent = parent
+        }
+
+        fun Child() = ParseNode(this)
     }
 
-    abstract val returnNodeValue: NodeValue<Return>
+    protected interface Parser {
+        fun parse(): Result
 
-    internal fun interface Parser<Input> {
-        fun parse(): ParseResult
+        data class Result(val succeed: Boolean)
     }
-
-    internal sealed interface ParseResult { val index: Int }
-    internal data class ParseSucceed(override val index: Int, val hasNext: Boolean): ParseResult
-    internal data class ParseFailure(override val index: Int): ParseResult
-
-    internal abstract fun createParser(input: List<Input>, index: Int): Parser<Input>
 
     companion object {
-        fun <Input> static(value: Input): Node<Input, Input, Unit> = TODO()
-        fun <Input> static(vararg value: Input): Node<Input, List<Input>, Unit> = TODO()
-        fun <Input> statics(iterable: Iterable<Input>): Node<Input, List<Input>, Unit> = TODO()
-        fun <Input, Return> dynamic(generator: () -> Node<Input, Return, *>): Node<Input, Return, Unit> = TODO()
-        fun <Input> conditional(condition: (Input) -> Boolean): Node<Input, Input, Unit> = TODO()
-        fun <Input> sequence(): Node<Input, List<Input>, Unit> = TODO()
-        fun <Input, Return, Error> optional(node: Node<Input, Return, Error>): Node<Input, Return?, Error> = TODO()
-        operator fun <Input> invoke(constructor: context(NodeConstructorContext<Input>, Companion) () -> Unit): Node<Input, Unit, Unit> =
+        operator fun <Type> invoke(constructor: context(ConstructorContext<Type>) () -> Unit): FSPNode<Type, Unit> {
             TODO()
-
-        operator fun <Input, Return> invoke(constructor: context(NodeConstructorContext<Input>, Companion) () -> NodeValue<Return>): Node<Input, Return, Unit> =
-            TODO()
-    }
-}
-
-sealed interface NodeValue<Type>
-
-internal class NodeScope {
-    val parent: NodeScope
-    val children: List<NodeScope> get() = children_
-    private val children_: MutableList<NodeScope> = LinkedList()
-
-    private constructor(parent: NodeScope) {
-        this.parent = parent
-    }
-
-    constructor() {
-        this.parent = this
-    }
-
-    fun appendScope(): NodeScope {
-        val child = NodeScope(this)
-        children_ += child
-        return child
-    }
-}
-
-internal class StaticNode<Input>(private val value: Input) : Node<Input, Input>() {
-    override fun createParser(input: List<Input>, index: Int) = Parser<Input> {
-        if (input[index] == value) return@Parser ParseResult(index, succeed = true, hasNext = false)
-        return@Parser ParseResult(index + 1, succeed = false, hasNext = false)
-    }
-}
-
-internal class StaticListNode<Input>(private val values: Array<Input>) : Node<Input, List<Input>>() {
-    override fun createParser(input: List<Input>, index: Int) = Parser<Input> {
-        var index = index
-        for (value in values) {
-            if (input[index] != value) return@Parser ParseResult(
-                index,
-                succeed = false,
-                hasNext = false
-            )
-            index += 1
         }
-        return@Parser ParseResult(index, succeed = true, hasNext = false)
+
+        operator fun <Type, Return> invoke(constructor: context(ConstructorContext<Type>) () -> FSPNodeReference<Return>): FSPNode<Type, Return> {
+            TODO()
+        }
+
+        fun <Type> Static(value: Type, failure: (() -> Throwable)? = null): FSPNode<Type, Type> = TODO()
+        fun <Type> Static(vararg value: Type, failure: (() -> Throwable)? = null): FSPNode<Type, List<Type>> = TODO()
+        fun <Type> Static(iterable: Iterable<Type>, failure: (() -> Throwable)? = null): FSPNode<Type, List<Type>> =
+            TODO()
+
+        fun <Type> Conditional(condition: (Type) -> Boolean, failure: (() -> Throwable)? = null): FSPNode<Type, Type> =
+            TODO()
+
+        fun <Type> Sequence(failure: (() -> Throwable)? = null): FSPNode<Type, List<Type>> = TODO()
+        fun <Type, Return> Optional(node: FSPNode<Type, Return>): FSPNode<Type, Return?> = TODO()
+    }
+
+    sealed class ConstructorContext<Type> {
+        abstract val append: AppendTarget<Type>
+
+        sealed interface AppendTarget<Type> {
+            infix fun <Return> node(node: FSPNode<Type, Return>)
+            infix fun task(task: () -> Unit)
+//            infix fun <Return> task(task: ImmutableReturnTask<Return>): NodeValueDelegate<Return>
+//            infix fun <Return> task(task: MutableReturnTask<Return>): NodeValueDelegate<Return>
+        }
+
+        fun static(value: Type, failure: (() -> Throwable)? = null): FSPNode<Type, Type> = TODO()
+        fun static(vararg value: Type, failure: (() -> Throwable)? = null): FSPNode<Type, List<Type>> = TODO()
+        fun static(iterable: Iterable<Type>, failure: (() -> Throwable)? = null): FSPNode<Type, List<Type>> = TODO()
+        fun <Return> dynamic(
+            constructor: context(ConstructorContext<Type>) () -> FSPNode<Type, Return>,
+            failure: (() -> Throwable)? = null
+        ): FSPNode<Type, Return> = TODO()
+
+        fun conditional(condition: (Type) -> Boolean, failure: (() -> Throwable)? = null): FSPNode<Type, Type> = TODO()
+        fun sequence(failure: (() -> Throwable)? = null): FSPNode<Type, List<Type>> = TODO()
+        fun <Return> optional(node: FSPNode<Type, Return>): FSPNode<Type, Return?> = TODO()
+    }
+
+    protected class ConstructorContextClass<Type>(processes: MutableList<Process>) : ConstructorContext<Type>() {
+        fun close() {
+            appendTarget.close()
+        }
+
+        private val appendTarget = AppendTargetClass<Type>(processes)
+        override val append: ConstructorContext.AppendTarget<Type> get() = appendTarget
+
+        private class AppendTargetClass<Type>(private var processes_: MutableList<Process>?) :
+            ConstructorContext.AppendTarget<Type> {
+            fun close() {
+                processes_ = null
+            }
+
+            private val processes get() = processes_!!
+
+            override fun <Return> node(node: FSPNode<Type, Return>) {
+                TODO("Not yet implemented")
+            }
+
+            override fun task(task: () -> Unit) {
+                TODO("Not yet implemented")
+            }
+        }
     }
 }
 
-internal class DynamicNode<Input>() : Node<Input, List<Input>>() {
-    override fun createParser(input: List<Input>, index: Int) = Parser<Input> {
 
-    }
-}
+sealed interface FSPNodeReference<Type>
+sealed interface FSPImmutableNodeReference<Type> : FSPNodeReference<Type>
+sealed interface FSPMutableNodeReference<Type> : FSPNodeReference<Type>
 
-internal class ConditionalNode<Input> : Node<Input, Input>() {
-    override fun createParser(input: List<Input>, index: Int) = Parser<Input> {
-        TODO("Not yet implemented")
-    }
-}
-
-internal class OptionalNode<Input, Return> : Node<Input, Return?>() {
-    override fun createParser(input: List<Input>, index: Int) = Parser<Input> {
-        TODO("Not yet implemented")
-    }
-}
-
-internal class ContainerNode<Input, Return>(private val tasks: List<Any>) : Node<Input, Return>() {
-    override fun createParser(input: List<Input>, index: Int) = Parser<Input> {
-        TODO("Not yet implemented")
-    }
-}
-
-class NodeConstructorContext<Input> internal constructor() {
-    val append = AppendTarget<Input>()
-
-    class AppendTarget<Input> {
-        infix fun <Return> node(node: Node<Input, Return>) {}
-        infix fun task(task: () -> Unit) {}
-    }
-}
-
-
-val testNode = Node<Char> {
-    append node static('a')
-    append node static('a', 'b')
-}
-
-fun test() {
-
-}
+//import java.util.*
+//import kotlin.reflect.KProperty
+//
+//sealed class FSPNode<Type, Return> {
+//    fun parse(input: Array<out Type>): Return = parse()
+//    fun parse(input: Iterable<Type>): Return = parse()
+//    fun parse(input: Iterator<Type>): Return = parse()
+//    private fun parse(): Return {
+//        val processor = Processor(processes)
+//        TODO()
+//    }
+//
+//    internal abstract val processes: List<Process>
+//
+//    internal fun interface Process {
+//        fun execute()
+//    }
+//
+//    internal class Processor {
+//        private val parent: Processor
+//        private val processes: List<Process>
+//
+//        constructor(processes: List<Process>) {
+//            this.parent = this
+//            this.processes = processes
+//        }
+//
+//        private constructor(parent: Processor, processes: List<Process) {
+//            this.parent = parent
+//            this.processes = processes
+//        }
+//
+//        fun Child(processes: List<Process>): Processor = Processor(this, processes)
+//    }
+//
+//    sealed interface ConstructorContext<Type> {
+//        val append: AppendTarget<Type>
+//
+//        sealed interface AppendTarget<Type> {
+//            infix fun <Return> element(node: FSPElement<Type, Return>): NodeValueDelegate<Return>
+//            infix fun task(task: () -> Unit)
+//            infix fun <Return> task(task: ImmutableReturnTask<Return>): NodeValueDelegate<Return>
+//            infix fun <Return> task(task: MutableReturnTask<Return>): NodeValueDelegate<Return>
+//        }
+//
+//        class NodeValueDelegate<Type> {
+//            operator fun getValue(thisRef: Any?, property: KProperty<*>): NodeValue<Type> {
+//                TODO()
+//            }
+//        }
+//    }
+//
+//    protected class ConstructorContextClass<Type>(processes: MutableList<Process>) : ConstructorContext<Type> {
+//        fun close() {
+//            appendTarget.close()
+//        }
+//
+//        private val appendTarget = AppendTargetClass<Type>(processes)
+//        override val append: ConstructorContext.AppendTarget<Type> get() = appendTarget
+//
+//        private class AppendTargetClass<Type>(private var processes_: MutableList<Process>?) :
+//            ConstructorContext.AppendTarget<Type> {
+//            fun close() {
+//                processes_ = null
+//            }
+//
+//            private val processes get() = processes_!!
+//
+//            override fun <Return> element(node: FSPElement<Type, Return>): ConstructorContext.NodeValueDelegate<Return> {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun task(task: () -> Unit) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun <Return> task(task: ImmutableReturnTask<Return>): ConstructorContext.NodeValueDelegate<Return> {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun <Return> task(task: MutableReturnTask<Return>): ConstructorContext.NodeValueDelegate<Return> {
+//                TODO("Not yet implemented")
+//            }
+//        }
+//    }
+//
+//    class Release<Type>(constructor: context(ConstructorContext<Type>, FSPElement.ConstructorContext<Type>) () -> Unit) :
+//        FSPNode<Type, Unit>() {
+//        override val processes: List<Process>
+//
+//        init {
+//            val processes = LinkedList<Process>()
+//            val context = ConstructorContextClass<Type>(processes)
+//            constructor(context, FSPElement.getConstructorContext())
+//            context.close()
+//            this.processes = processes
+//        }
+//
+//
+//    }
+//
+//    class Capture<Type, Return>(constructor: context(ConstructorContext<Type>, FSPElement.ConstructorContext<Type>) () -> NodeValue<Return>) :
+//        FSPNode<Type, Return>() {
+//        override val processes: List<Process>
+//        private val returnReference: NodeValue<Return>
+//
+//        init {
+//            val processes = LinkedList<Process>()
+//            val context = ConstructorContextClass<Type>(processes)
+//            this.returnReference = constructor(context, FSPElement.getConstructorContext())
+//            context.close()
+//            this.processes = processes
+//        }
+//    }
+//}
+//
+//sealed interface NodeValue<Type>
+//sealed interface ImmutableNodeValue<Type> : NodeValue<Type>
+//sealed interface MutableNodeValue<Type> : NodeValue<Type>
+//
+//sealed class FSPElement<Input, Return> {
+//    protected abstract val returnNodeValue: ImmutableNodeValue<Return>
+//    abstract fun <Class : Throwable> attachThrowable(block: () -> Class): FSPElement<Input, Return>
+//
+//    internal class Partition<Input> {
+//        val parent: Partition<Input>
+//        val parser: Parser<Input>
+//
+//        constructor(parser: Parser<Input>) {
+//            this.parent = this
+//            this.parser = parser
+//        }
+//
+//        private constructor(parent: Partition<Input>, parser: Parser<Input>) {
+//            this.parent = parent
+//            this.parser = parser
+//        }
+//
+//        fun createChild(parser: Parser<Input>) = Partition(this, parser)
+//
+//        interface Parser<Input> {
+//            fun parse(): Result
+//
+//            sealed interface Result {
+//                val index: Int
+//
+//                data class Succeed(override val index: Int, val hasNext: Boolean) : Result
+//                data class Failure(override val index: Int) : Result
+//            }
+//        }
+//    }
+//
+//    internal abstract fun createPartitionParser(input: List<Input>, index: Int): Partition.Parser<Input>
+//
+//    sealed class ConstructorContext<Type> {
+//        fun static(value: Type, failure: (() -> Throwable)? = null): FSPElement<Type, Type> = TODO()
+//        fun static(vararg value: Type, failure: (() -> Throwable)? = null): FSPElement<Type, List<Type>> = TODO()
+//        fun static(iterable: Iterable<Type>, failure: (() -> Throwable)? = null): FSPElement<Type, List<Type>> = TODO()
+//        fun <Return> dynamic(
+//            constructor: context(FSPNode.ConstructorContext<Type>, ConstructorContext<Type>) () -> FSPElement<Type, Return>,
+//            failure: (() -> Throwable)? = null
+//        ): FSPElement<Type, Return> = TODO()
+//
+//        fun conditional(condition: (Type) -> Boolean, failure: (() -> Throwable)? = null): FSPElement<Type, Type> =
+//            TODO()
+//
+//        fun sequence(failure: (() -> Throwable)? = null): FSPElement<Type, List<Type>> = TODO()
+//        fun <Return> optional(node: FSPElement<Type, Return>): FSPElement<Type, Return?> = TODO()
+//
+//        fun <Return> imut(block: () -> Return): ImmutableReturnTask<Return> {
+//            TODO()
+//        }
+//
+//        fun <Return> mut(block: () -> Return): MutableReturnTask<Return> {
+//            TODO()
+//        }
+//    }
+//
+//    private data object ConstructorContextObject : ConstructorContext<Nothing>()
+//
+//    internal companion object {
+//        fun <Input> getConstructorContext() =
+//            @Suppress("UNCHECKED_CAST") (ConstructorContextObject as ConstructorContext<Input>)
+//    }
+//}
+//
+//
+//internal sealed interface Task<Return>
+//class ImmutableReturnTask<Return> : Task<Return>
+//class MutableReturnTask<Return> : Task<Return>
+//
+//
+//val testNode = FSPNode.Release<Char> {
+//    append element static('a')
+//    val ab by append element static('a', 'b')
+//    val test by append task imut { mutableListOf<Int>() }
+//    val test2 by append task mut { }
+//    append task {
+//        println("test")
+//    }
+//}
